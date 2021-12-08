246045410fc8a4798bee0a620c27d7c57024ea9b
Joseph <joseph300905@gmail.com>
2021-12-08 01:07:32 +0100

    Icons

--- .config/polybar/polybar.sh
+++ .config/polybar/polybar.sh
@@ -4,6 +4,8 @@ pkill polybar
 # sleep 1;
 # polybar i3wmthemer_bar &
 
+DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload i3bar &
