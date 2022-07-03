380bf8707303289d4d3a3813b25be77b1cdf19cf
Joseph <joseph300905@gmail.com>
2022-07-03 12:30:36 +0200

    Update polybar

--- .config/polybar/polybar.sh
+++ .config/polybar/polybar.sh
@@ -1,16 +1,6 @@
-#!/bin/env sh
-
+#!/bin/env bash
 pkill polybar
-# sleep 1;
-# polybar i3wmthemer_bar &
-
-DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
-
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload i3bar &
-  done
-else
-  polybar --reload example &
-fi
+done
 
