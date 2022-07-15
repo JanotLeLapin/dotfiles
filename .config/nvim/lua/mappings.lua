552e1623b6073f0725b15ff5baf3edb72ba253f5
Joseph <joseph300905@gmail.com>
2022-07-15 21:56:12 +0200

    Hover

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,5 +7,6 @@ return function(client, bufnr)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', 'rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 end
 
